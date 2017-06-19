0139f955fefbf46b8d8ebf5eaad2fc15d1fa1a65
JasonLai0825 <jasongood3312@gmail.com>
2017-06-19 23:34:28 +0800

    Add files via upload

---
+++ windowserviceTest/Controllers/HomeController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web.Http;
+
+namespace windowserviceTest.Controllers
+{
+    public class HomeController : ApiController
+    {
+        [Route("api/home")]
+        [HttpGet]
+        public IHttpActionResult Test()
+        {
+            return Ok("test is success.");
+        }
+    }
+}
